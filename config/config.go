b216f5c47f8ce035a70bfc7b0bde8d938edb3109
Eden Reich <eden.reich@gmail.com>
2025-08-20 22:00:03 +0200

    refactor(git): Update git push command pattern to allow optional space before origin
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- config/config.go
+++ config/config.go
@@ -229,7 +229,7 @@ func DefaultConfig() *Config { //nolint:funlen
 						"^git status$",
 						"^git log --oneline -n [0-9]+$",
 						"^git commit -m \".+\"$",
-						"^git push( --set-upstream origin)?( (?!main$|master$)[a-zA-Z0-9/_-]+)?$",
+						"^git push( --set-upstream| origin)?( (?!main$|master$)[a-zA-Z0-9/_-]+)?$",
 					},
 				},
 			},
